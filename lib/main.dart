497e74dbf7501a86a08e4b0229cf4052d185f8a3
jbhangugstar <jbhangugstar@gmail.com>
2026-04-02 14:52:09 +0700

    progress1

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:mypresensi/database/preference.dart';
 import 'view/login_screen.dart';
-import 'view/register_screen.dart';
-import 'view/profile_screen.dart';
-import 'view/edit_profile_screen.dart';
-import 'model/get_user_model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PreferenceHandler().init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Presensi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegisterScreen(),
-        '/profile': (context) => const ProfileScreen(),
-        '/edit_profile': (context) => EditProfileScreen(
-          user: ModalRoute.of(context)!.settings.arguments as GetUserData,
-        ),
-      },
+      home: const LoginScreen(),
     );
   }
 }
